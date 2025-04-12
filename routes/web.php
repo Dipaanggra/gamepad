a1ebf1cfb72299b40677d2c61843a98eaa715d38
dipaanggra <putriwahyuangraini18@gmail.com>
2025-04-12 08:16:48 +0700

    menambahkan route version

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\GameVersionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Route::group(function () {
-    Route::get('/', [\App\Http\Controllers\User\GameController::class, 'welcome'])->name('welcome');
-    Route::get('/games', [\App\Http\Controllers\User\GameController::class, 'index'])->name('user.game');
-    Route::get('/games/{game}', [\App\Http\Controllers\User\GameController::class, 'show'])->name('user.game.show');
+Route::get('/', [\App\Http\Controllers\User\GameController::class, 'welcome'])->name('welcome');
+Route::get('/games', [\App\Http\Controllers\User\GameController::class, 'index'])->name('user.game');
+Route::get('/games/{game:slug}', [\App\Http\Controllers\User\GameController::class, 'show'])->name('user.game.show');
 // });
 
 
@@ -23,7 +24,15 @@ Route::middleware('auth')->group(function () {
         ->group(function () {
             Route::get('/', DashboardController::class)->name('dashboard');
             // Game Dashboard
-            Route::resource('game', GameController::class);
+            Route::resource('game', GameController::class)->except('show');
+            Route::get('game/{game:slug}', [GameController::class, 'show'])->name('game.show');
+
+            // Game Version (nested in Game by slug)
+            Route::prefix('game/{game:slug}')->group(function () {
+                Route::resource('version', GameVersionController::class)->except('index');
+            });
+
+
             // User Dashboard
             Route::resource('user', UserController::class)
                 ->middleware('role:admin')
